47f94c6764f973daddcfb9928682471c4b59956c
LordAldren (LordAldren) <lordaldren.silvano07@gmail.com>
2025-09-16 20:51:08 +0800

    .

--- backend/sql/db.php
+++ backend/sql/db.php
@@ -4,9 +4,13 @@ $dbname = "admin_admin";
 $username = "admin_admin";
 $password = "123";
 
-$conn = new mysqli($host, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    // Set error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Optional: use associative arrays by default
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>

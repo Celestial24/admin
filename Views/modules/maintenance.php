17fdf667f82d395641b9343a2002a2056a8d86c8
LordAldren <lordaldren.silvano07@gmail.com>
2025-09-22 01:21:32 +0800

    .

--- Views/modules/maintenance.php
+++ Views/modules/maintenance.php
@@ -70,7 +70,7 @@ if (!$maintenance_result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Maintenance - Who Reported</title>
+    <title>Maintenance</title>
     <link rel="icon" type="image/png" href="../../assets/image/logo2.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>

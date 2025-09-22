31846d94d02a78187b4aa950065425b138e8bbf3
LordAldren <lordaldren.silvano07@gmail.com>
2025-09-22 10:57:05 +0800

    .

--- Components/sidebar/sidebar_user.php
+++ Components/sidebar/sidebar_user.php
@@ -91,7 +91,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
             </div>
 
             <!-- Visitor Logs -->
-            <a href="/admin/user/visitors.php" 
+            <a href="/admin/user/Visitors.php" 
                class="flex items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-700 <?= $currentPage === 'visitors.php' ? 'bg-gray-700' : '' ?>">
                 <i data-lucide="users" class="w-5 h-5"></i>
                 <span class="sidebar-text">Visitor Logs</span>
